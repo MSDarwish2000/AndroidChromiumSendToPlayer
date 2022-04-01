132eeeb3b95d69356d44aa176152c08cdb798b42
Mohamed Darwish <msdarwish.2000@outlook.com>
2022-04-01 15:42:27 +0200

    Delete v2 and move v3

--- v3/data/copy/index.js
+++
@@ -1,8 +0,0 @@
-const args = new URLSearchParams(location.search);
-
-const copy = () => navigator.clipboard.writeText(args.get('content')).then(() => {
-  setTimeout(() => window.close(), 1000);
-});
-
-copy();
-document.getElementById('copy').addEventListener('click', copy);
